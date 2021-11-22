30da80d1fd2e284ce0918ad3ea8dd95868eb4508
trimagnus <tri_magnus@hotmail.com>
2021-11-22 10:38:12 -0800

    Updated page styles

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,7 @@
 export default function makeFooter() {
   const footer = document.createElement('footer');
-  footer.innerHTML = `<p>This is the footer area</p>`;
-
+  const ghImgSrc = "../node_modules/super-tiny-icons/images/svg/github.svg";
+  footer.innerHTML = `<p>&copy 2021 Joshua Cahoon</p>`;
+  footer.innerHTML += `<a href="https://github.com/trimagnus"><img width="20px" height="20px" class="ghIcon" src="${ghImgSrc}"></img></a>`
   return footer;
 }
--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,14 @@ export default function makeMenuPage() {
   const menu = document.createElement('div');
   menu.innerHTML = `<p>This is the menu page</p>`;
   
-
+  const menuData = [
+    {
+      title: 'Chicken Parmesean',
+      description: 'blah',
+      price: '$10.99'
+    },
+    {}
+  ];
 
   return menu;
 }
