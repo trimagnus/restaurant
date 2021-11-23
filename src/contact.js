89ef8bb98d2d0f7cb42bb5862fbf235d41f2bea8
trimagnus <tri_magnus@hotmail.com>
2021-11-23 05:17:39 -0800

    Updated contact page

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,15 @@
 export default function makeContactPage() {
   const contact = document.createElement('div');
-  contact.innerHTML = `<p>This is the contact page</p>`;
+  contact.innerHTML = `
+  <div class="contact">
+    <p>We will cater your next gathering, call us today!</p>
+    <p class="contactSection">Restaurant</p>
+    <p>5555 Random Drive</p>
+    <p>Distant, CA 99999</p>
+    <p class="contactSection">Phone: (555) 555-5555</p>
+    <p>Fax: (555) 111-5151</p>
+  </div>
+  `;
 
   return contact;
 }
