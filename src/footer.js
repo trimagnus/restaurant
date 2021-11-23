57c87b43a4c74bf8e40e4992e41e4922f46dbc7e
trimagnus <tri_magnus@hotmail.com>
2021-11-23 05:38:49 -0800

    Fixed github footer icon

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,6 @@
 export default function makeFooter() {
   const footer = document.createElement('footer');
-  const ghImgSrc = "../node_modules/super-tiny-icons/images/svg/github.svg";
+  const ghImgSrc = "./img/GitHub-Mark-Light-64px.png";
   footer.innerHTML = `<p>&copy 2021 Joshua Cahoon</p>`;
   footer.innerHTML += `<a href="https://github.com/trimagnus"><img width="20px" height="20px" class="ghIcon" src="${ghImgSrc}"></img></a>`
   return footer;
